639d31b771833583ccec8f406c2572ef031f2237
Zekumoru Dragonhart <dev@zekumoru.com>
2023-03-16 01:49:34 +0100

    Add global styles

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    background-color: #18191a;
+    color: #e6e6e6;
+    color-scheme: dark;
+  }
+`;
+
+export default GlobalStyles;
