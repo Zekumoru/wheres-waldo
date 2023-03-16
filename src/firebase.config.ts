441f8900bc14e6806755dc62792f9fdcbd0f50d9
Zekumoru Dragonhart <dev@zekumoru.com>
2023-03-16 13:43:53 +0100

    Install and setup firebase

---
+++ src/firebase.config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAQW4Xe2DIH6qYrRnA8NaP6rk2zbmqDXOk',
+  authDomain: 'wheres-waldo-zekumoru.firebaseapp.com',
+  projectId: 'wheres-waldo-zekumoru',
+  storageBucket: 'wheres-waldo-zekumoru.appspot.com',
+  messagingSenderId: '322128104865',
+  appId: '1:322128104865:web:92d713f53596af7b950fe0',
+};
+
+export default firebaseConfig;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { initializeApp } from 'firebase/app';
 import App from './App';
 import GlobalStyles from './GlobalStyles';
+import firebaseConfig from './firebase.config';
+
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
