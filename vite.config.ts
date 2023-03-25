90c6f731e0ef1b4fb170a13d309dd5b6f5fccb38
Zekumoru Dragonhart <dev@zekumoru.com>
2023-03-25 17:58:34 +0100

    Split packages into chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+const packages = ['firebase'];
+const manualChunks = (id: string) => {
+  if (!id.includes('node_modules')) return;
+
+  const packageName = packages.find((p) => id.includes(p));
+  if (packageName) {
+    return `vendor-${packageName}`;
+  }
+
+  return 'vendor';
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks,
+      },
+    },
+  },
+});
